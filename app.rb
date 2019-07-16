0169a4074784b90ca5bf4f090ebc8c49e773036e
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-15 18:02:09 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do 
+    "#{params[:name].reverse}"
+  end 
+
+  get '/square/:number' do 
+    "#{params[:number] * params[:number]}"
+  end
 
 end
